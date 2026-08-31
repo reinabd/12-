f3697c165e8b2e3c093d8256aefb8627bf7bb176
reinabd <reinabd@proton.me>
2026-08-31 20:20:33 +0300

    Change countdown target year to 2038

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(` October 08 , 2026 00:00:00 
+const newYearTime = new Date(` October 08 , 2038 00:00:00 
 `);
 
 // Update countdown time
